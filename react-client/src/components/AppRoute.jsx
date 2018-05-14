706e78bd0d5b4b79eafd05cae7744cf0b64b749c
afaqsmadi <afaqsmadi@gmail.com>
2018-05-14 11:42:42 +0300

    delet bundel

--- react-client/src/components/AppRoute.jsx
+++ react-client/src/components/AppRoute.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import $ from 'jquery'
 import { BrowserRouter, Route, Link, IndexRoute, hashHistory, browserHistory, Switch } from 'react-router-dom'
 import { HashRouter } from 'react-router-dom'
-import SignUp from './SignUp.js'
+
 import Products from './Products.jsx'
 import SignIn from './SignIn.js'
 
+
+import SignUp from './SignUp.jsx';
+import Navcom from './navbar.jsx';
 
 class AppRoute extends React.Component {
   constructor (props) {
@@ -22,6 +25,7 @@ class AppRoute extends React.Component {
           <Route exact path='/signup' component={SignUp} />
           <Route exact path='/prouducts' component={Products} />
             <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/' component={Navcom}/>
         </Switch>
       </BrowserRouter>
     )
