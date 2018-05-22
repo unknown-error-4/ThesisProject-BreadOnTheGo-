691a35d09bb16a590513604c461dca9b9f20c382
afaqsmadi <afaqsmadi@gmail.com>
2018-05-22 22:53:58 +0300

    navbar change

--- react-client/src/components/navbar.jsx
+++ react-client/src/components/navbar.jsx
@@ -7,60 +7,53 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-
-import SignIn from './SignIn.jsx';
-import SignUp from './SignUp.jsx';
+import SignIn from './SignIn.js';
+import SignUp from './SignUp.js';
 import Profile from './Profile.jsx';
- // import styles from  './navbar.css'
-
+import SearchProduct from './SearchProduct.jsx' 
 class Navcom extends React.Component {
   constructor (props) {
     super(props)
-
+   
   }
   render(){
   	return(
-      <Router>
-         <div >
-      <Navbar id='navb' >
-         <Navbar.Header>
-        <Navbar.Brand>
-      <a href="#brand">BreadOnTheGo</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-       </Navbar.Header>
-       <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} >
-        Home
-      </NavItem>
-      <NavItem eventKey={2}>
-
-      </NavItem>
+    <Router>
+       <div > 
+    <Navbar id='navb' >
+      <Navbar.Header>
+      <Navbar.Brand>
+    <a href="#brand">BreadOnTheGo</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+     </Navbar.Header>
+     <Navbar.Collapse>
+  <Nav>
+    <NavItem eventKey={1} >
+      Home
+    </NavItem>
+    <NavItem eventKey={2}>
+       <Link to= "/SearchProduct">product</Link>
+    </NavItem>
+  </Nav>
+  <Nav pullRight>
+  <NavItem eventKey={1}>
+       <Link to= "/Profile">Profile</Link>
+    </NavItem>
       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
-    </Nav>
-    <Nav pullRight>
-    <NavItem eventKey={1}>
-         <Link to= "/Profile">Profile</Link>
-      </NavItem>
-      <NavItem eventKey={1}>
-         <Link to= "/SignIn">SignIn</Link>
-      </NavItem>
-      <NavItem eventKey={2} >
-       <Link to= "/SignUp">SignUp</Link>
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
+    <MenuItem eventKey={3.1}><Link to= "/SignIn">SignIn</Link></MenuItem>
+    <MenuItem eventKey={3.2}><Link to= "/SignUp">SignUp</Link></MenuItem>
+    <MenuItem eventKey={3.3}>logOut</MenuItem>
+    <MenuItem divider />
+    </NavDropdown>
+
+  </Nav>
+</Navbar.Collapse>
 </Navbar>
-      <Route path="/Profile"  component={Profile}/>
-      <Route path="/SignIn"  component={SignIn}/>
-      <Route path="/SignUp" component={SignUp} />
+    <Route path="/Profile"  component={Profile}/>
+    <Route path="/SignIn"  component={SignIn}/>
+    <Route path="/SignUp" component={SignUp} />
+    <Route path='/SearchProduct' component={SearchProduct}/>
 </div>
 </Router>
   		)
