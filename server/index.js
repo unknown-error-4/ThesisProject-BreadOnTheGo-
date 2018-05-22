0c743c9196a615b0f06e6939177b3e87a21b3dae
afaqsmadi <afaqsmadi@gmail.com>
2018-05-22 22:56:46 +0300

    change of server

--- server/index.js
+++ server/index.js
@@ -4,12 +4,10 @@ var bodyParser = require('body-parser');
 var handler =require('./handler');
 var path = require('path');
 var items = require('../database-mongo');
-
 var app = express();
+// var multer  =   require('multer');
 //to connect with react
 app.use(express.static(__dirname + '/../react-client/dist'));
-
-
 /////////////////////////////////////////////////////////////
 
 app.use(bodyParser.json());
@@ -18,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // session ::
     // authinticate transzction between Server and client ..
     app.use(session({
-        secret: 'any string of text',
+     secret: 'any string of text',
      resave: true, //even if nothing changed in the files ,, gana save it again ..
      saveUninitialized: false // for the database
     }));
@@ -26,16 +24,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/signup', handler.SignUp);
 app.post('/signin',handler.SignIn);
 app.post("/prouducts",handler.SavingProducts)
-app.get("/profile",handler.retrieveOne)
+app.get("/profile",handler.retrieveOne) 
 app.get("/showProduct",handler.showProduct)
 app.get("/showOne",handler.retrieveOneProduct)
 app.get("/profiles",handler.retrieve)
-app.get('/',handler.signout)
+app.post("/upload",handler.upload)
+app.get("/upload",handler.getImage)
+// app.get("/upload",handler.updateImage)
 app.get('/*', (req, res) => {
  res.sendFile(path.resolve(path.join(__dirname, '/../react-client/dist/index.html')));
 });
-
-/////////////////////////////////////////////////////////////
+/////////////////////////////
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
