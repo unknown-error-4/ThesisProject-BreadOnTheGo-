3abe3bf28a88ee09d3667ffb3532dcf46b39a4a4
FatimaAlmostafa <ff242468@gmail.com>
2018-05-29 14:02:33 +0300

    fixerror

--- react-client/src/components/Rating.jsx
+++ react-client/src/components/Rating.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StarRatingComponent from 'react-star-rating-component';
- 
+
 class Rating extends React.Component{
   constructor(props) {
     super(props);
@@ -10,12 +10,12 @@ class Rating extends React.Component{
       product:[]
     };
   }
- 
+
   onStarClick(nextValue, prevValue, name) {
     var that=this
     $.ajax({
       url:'./updateRating',
-      type: 'PUT',    
+      type: 'PUT',
       data: JSON.stringify({rating: nextValue}),
       contentType: 'application/json',
       success: function(result) {
@@ -24,7 +24,7 @@ class Rating extends React.Component{
           })
           this.setState({rating: nextValue});
   }
-  //when i make refresh for the data i need to save 
+  //when i make refresh for the data i need to save
   componentDidMount(){
     var that=this
      var product = this.props.rating
@@ -33,7 +33,7 @@ class Rating extends React.Component{
         type:'GET',
         success:function(data){
           console.log("dataaaaaa",data)
-          console.log(typeof data) 
+          console.log(typeof data)
           for(var key in data){
 
             that.setState({rating: data[key].rating});
@@ -44,12 +44,12 @@ class Rating extends React.Component{
   }
   render() {
     var { rating } = this.state;
-    
-    return (                
+
+    return (
       <div>
         <p>Rating from state: {rating}</p>
-        <StarRatingComponent 
-          name="rate1" 
+        <StarRatingComponent
+          name="rate1"
           starCount={10}
           value={rating}
           onStarClick={this.onStarClick.bind(this)}
